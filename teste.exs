0197587ba501f04ff5aa3d60036487b7d51b55de
Vinicius Dias <carlosv775@gmail.com>
2021-08-28 19:26:15 -0300

    Video 4.3

--- teste.exs
+++ teste.exs
@@ -1,5 +1,14 @@
 defmodule MeuModulo do
+    import IO, only: [puts: 1]
+    import Kernel, except: [inspect: 1]
+
     def ola_mundo do
-        IO.puts("Olá, mundo!")
+        inspect("Olá, mundo")
+    end
+
+    def inspect(parametro) do
+        puts("Começando a inspeção")
+        puts(parametro)
+        puts("Terminando a inspeção")
     end
 end
