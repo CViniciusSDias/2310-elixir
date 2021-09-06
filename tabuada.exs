424f949763c3bfe713df828c88a0d041e825a950
Vinicius Dias <carlosv775@gmail.com>
2021-09-06 14:13:56 -0300

    Video 2.1

---
+++ tabuada.exs
@@ -0,0 +1,11 @@
+defmodule MeuModulo.Tabuada do
+    def calcula(multiplicador) do
+        tabuada(multiplicador, 1)
+    end
+
+    defp tabuada(_, 11), do: []
+
+    defp tabuada(produto1, produto2) do
+        [produto1 * produto2] ++ tabuada(produto1, produto2 + 1)
+    end
+end
